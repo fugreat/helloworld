8973750e8afd3b9d69f07995d35de3363da6d438
Chris Bagwell <chris.bagwell@us.fujitsu.com>
2015-03-16 14:37:28 -0500

    Improve code coverage.

--- test/hello_test.cc
+++ test/hello_test.cc
@@ -2,17 +2,38 @@
  * @file hello_test.cc
  * @brief hello Unit Tests
  */
-#include <assert.h>
 #include "../src/hello.h"
 #include "gtest/gtest.h"
 
 class hello_test : public ::testing::Test {
 };
 
+TEST_F(hello_test, copy)
+{
+    hello h1;
+    hello h2(h1);
+
+    std::string s = h2.get_hello();
+
+    ASSERT_EQ(s, "Hello, World!\n");
+}
+
+TEST_F(hello_test, assign)
+{
+    hello h1;
+    hello h2;
+
+    h2 = h1;
+
+    std::string s = h2.get_hello();
+
+    ASSERT_EQ(s, "Hello, World!\n");
+}
+
 TEST_F(hello_test, get_hello)
 {
     hello h;
     std::string s = h.get_hello();
 
-    assert(s == "Hello, World!\n");
+    ASSERT_EQ(s, "Hello, World!\n");
 }
--- test/world_test.cc
+++ test/world_test.cc
@@ -2,18 +2,38 @@
  * @file world_test.cc
  * @brief world Unit Tests
  */
-#include <assert.h>
-#include <string>
 #include <world.h>
 #include "gtest/gtest.h"
 
 class world_test : public ::testing::Test {
 };
 
+TEST_F(world_test, copy)
+{
+    world w1;
+    world w2(w1);
+
+    std::string s = w2.get_world();
+
+    ASSERT_EQ(s, "World");
+}
+
+TEST_F(world_test, assign)
+{
+    world w1;
+    world w2;
+
+    w2 = w1;
+
+    std::string s = w2.get_world();
+
+    ASSERT_EQ(s, "World");
+}
+
 TEST_F(world_test, get_world)
 {
     world w;
     std::string s = w.get_world();
 
-    assert(s == "World");
+    ASSERT_EQ(s, "World");
 }
